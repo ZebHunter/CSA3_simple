37f56217d06d1fc2df925fa2240a59d5bd1ca1fa
ZebHunter <mihailrogachev@niuitmo.ru>
2024-03-09 01:17:52 +0300

    Init and test

---
+++ interpreter/translator.py
@@ -0,0 +1,227 @@
+from __future__ import annotations
+
+import re
+import sys
+
+from machine.isa import Opcode, Word, write_code
+
+USER_REGISTERS = ["r1", "r2", "r3", "r4", "r5", "r6", "r7", "r8", "r9", "r10", "r11", "r12", "r14", "r15"]
+
+
+def is_register(arg: str) -> bool:
+    return arg in USER_REGISTERS
+
+
+def remove_extra_spaces(line: str) -> str:
+    line = line.replace("' '", str(ord(" ")))
+    return re.sub(r"\s+", " ", line)
+
+
+def remove_commas(line: str) -> str:
+    line = line.replace("','", str(ord(",")))
+    return line.replace(",", " ")
+
+
+def preprocessing(asm_text: str) -> str:
+    lines: list[str] = asm_text.splitlines()
+    strip_lines = map(str.strip, lines)
+    remove_empty_lines = filter(bool, strip_lines)
+    removed_commas = map(remove_commas, remove_empty_lines)
+    remove_spaces = map(remove_extra_spaces, removed_commas)
+    joined: str = "\n".join(remove_spaces)
+    return joined
+
+
+def transform_data_into_structure(data: str) -> (list[Word], dict[str, int]):
+    program_data: list[Word] = []
+    variables: dict[str, int] = {}
+    address_counter = 2
+
+    for line in data.split("\n"):
+        line = line.strip()
+        var_description = line.split(":")
+        assert len(var_description) == 2 or len(var_description) == 4, "Incorrect assignment"
+        name, value = var_description[0], var_description[1].strip()
+        assert name[0][-1] != " ", 'Spaces after variable name are not allowed, add ":"'
+        assert name not in variables, "Variable already defined."
+
+        if value[0] == '"':
+            ascii_values = [ord(char) for char in value[1:-1].replace(r"\n", "\n")]
+            ascii_values.insert(0, len(ascii_values))
+            variables[name] = address_counter
+            for char in ascii_values:
+                word = Word(int(address_counter), Opcode.NOP, int(char), 0)
+                program_data.append(word)
+                address_counter += 1
+        else:
+            variables[name] = address_counter
+            word = Word(int(address_counter), Opcode.NOP, int(value), 0)
+            program_data.append(word)
+            address_counter += 1
+
+    return program_data, variables
+
+
+def transform_text_into_structure(
+        text: str
+) -> (int, list[Word]):
+    assert text.find(".start:") != -1, ".start label not found"
+    labels: dict[str, int] = {}
+    start_address: int = -1
+    address_counter: int = 2
+    command_mem: list[Word] = []
+
+    # Label handling
+    for instr in text.split("\n"):
+        decoding = instr.split(" ")
+        if decoding[0][0] == ".":
+            current_label = decoding[0]
+            assert len(decoding) == 1, "Error parsing label."
+            assert current_label[-1] == ":", 'Label format error, excepted ":" mark'
+            assert current_label.find(":"), 'Label format error, excepted ":" mark'
+            current_label = decoding[0][1:-1]
+            if current_label == "start":
+                start_address = address_counter
+            labels[current_label] = address_counter
+        else:
+            address_counter += 1
+
+    address_counter = 2
+
+    # Instruction handling
+    for instr in text.split("\n"):
+        decoding = instr.split(" ")
+        if decoding[0][0] != ".":
+            assert Opcode(decoding[0].upper()) is not None, "No such opcode"
+            cur_opcode = Opcode(decoding[0].upper())
+            current_instruction = None
+            command_arguments = decoding[1:]
+
+            if cur_opcode in [Opcode.HALT, Opcode.NOP]:
+                assert len(command_arguments) == 0, "HLT/NOP should not have arguments"
+                current_instruction = Word(address_counter, cur_opcode, 0, 0)
+
+            if cur_opcode in [Opcode.JE, Opcode.JUMP, Opcode.JG,
+                              Opcode.JLE, Opcode.JL, Opcode.JNE,
+                              Opcode.JG, Opcode.JGE]:
+                assert len(command_arguments) == 1, "branch instruction should have 1 argument - label"
+                assert labels[command_arguments[0][1:]] is not None, "No such label"
+                current_instruction = Word(address_counter, cur_opcode, str(labels[command_arguments[0][1:]]), 0)
+
+            if cur_opcode == Opcode.MV:
+                assert len(command_arguments) == 2, "MV should have arguments two arguments"
+                assert is_register(command_arguments[0]), "MV first argument should be register"
+                if is_register(command_arguments[1]):
+                    current_instruction = Word(address_counter, cur_opcode, command_arguments[0], command_arguments[1])
+                else:
+                    raise Exception("MV second argument can be: register")
+
+            if cur_opcode in [Opcode.INC, Opcode.DEC]:
+                assert len(command_arguments) == 1, "INC/DEC/NEG must have only one argument - register"
+                assert is_register(command_arguments[0]), "INC/DEC/NEG first argument should be register"
+                current_instruction = Word(address_counter, cur_opcode, command_arguments[0], 0)
+
+            if cur_opcode in [Opcode.ADD, Opcode.MOD, Opcode.DIV,
+                              Opcode.SUB, Opcode.CMP, Opcode.AND,
+                              Opcode.OR, Opcode.XOR]:
+                assert len(command_arguments) == 2, "Arithmetic commands should have two registers as args"
+                assert is_register(command_arguments[0]), "Arithmetic commands args is registers"
+                current_instruction = Word(address_counter, cur_opcode, command_arguments[0], command_arguments[1])
+
+            if cur_opcode == Opcode.LD:
+                assert len(command_arguments) == 2, "LD must have 2 arguments"
+                assert is_register(command_arguments[0]), "Not registers in arguments"
+                if is_register(command_arguments[1]):
+                    current_instruction = Word(address_counter, cur_opcode, command_arguments[0], command_arguments[1])
+                elif command_arguments[1][0] == '[':
+                    command_arguments[1] = command_arguments[1][1:-1]
+                    current_instruction = (
+                        Word(address_counter, Opcode.LD_ADDR, command_arguments[0], command_arguments[1]))
+                else:
+                    current_instruction = (
+                        Word(address_counter, Opcode.LD_LIT, command_arguments[0], command_arguments[1]))
+
+            if cur_opcode == Opcode.ST:
+                assert len(command_arguments) == 2, "ST must have 2 arguments"
+                assert is_register(command_arguments[0]), "Not registers in arguments"
+                if is_register(command_arguments[1]):
+                    current_instruction = Word(address_counter, cur_opcode, command_arguments[0], command_arguments[1])
+                elif command_arguments[1][0] == '[':
+                    command_arguments[1] = command_arguments[1][1:-1]
+                    current_instruction = (
+                        Word(address_counter, Opcode.ST_ADDR, command_arguments[0], command_arguments[1]))
+
+            assert current_instruction is not None, "Instruction parsing error"
+            command_mem.append(current_instruction)
+            address_counter += 1
+
+    return start_address, command_mem
+
+
+def compilation(command_mem: list[Word], data_mem: list[Word], variables: dict[str, int]) -> list[Word]:
+    for word in data_mem:
+        word.index += len(command_mem)
+    for word in command_mem:
+        if word.opcode == Opcode.LD_ADDR or word.opcode == Opcode.LD_LIT or word.opcode == Opcode.ST_ADDR:
+            if word.arg2 in variables:
+                word.arg2 = variables[str(word.arg2)] + len(command_mem)
+            elif word.arg2 == "INP":
+                word.arg2 = 0
+            elif word.arg2 == "OUT":
+                word.arg2 = 1
+    return command_mem + data_mem
+
+
+def transform_to_instruction(code: dict) -> list[Word]:
+    memory: list[Word] = []
+    for key in code.keys():
+        if key == "code_mem":
+            for item in code[key]:
+                memory.append(item)
+        elif key == "data_mem":
+            count = len(code["code_mem"])
+            for item in code[key]:
+                item.set_word(item.index + count)
+                memory.append(item)
+    return memory
+
+
+def perform_translator(source: str, target: str) -> None:
+    code = preprocessing(source)
+    data_mem: list[Word] = []
+    variables = {}
+
+    text_index = code.find("section .text")
+
+    assert text_index != -1, "No .text section"
+    text_start, text_stop = text_index + len("section .text") + 1, None
+    data_index = code.find("section .data")
+    if data_index == -1:
+        text_stop = len(code)
+    else:
+        data_start, data_stop = data_index + len("section .data") + 1, None
+        if data_index < text_index:
+            data_stop = text_index - 1
+            text_stop = len(code)
+        else:
+            text_stop = data_index - 1
+            data_stop = len(code)
+        data_mem, variables = transform_data_into_structure(code[data_start:data_stop])
+
+    start, command_mem = transform_text_into_structure(code[text_start:text_stop])
+    memory = compilation(command_mem, data_mem, variables)
+    write_code(target, memory)
+
+
+def main(args):
+    assert len(args) == 2, "Usage: translation.py <source> <output>"
+    source = args[0]
+    target = args[1]
+    with open(source, encoding="utf-8") as file:
+        code = file.read()
+    perform_translator(code, target)
+
+
+if __name__ == "__main__":
+    sys.path.append("")
+    main(sys.argv[1:])
---
+++ machine/emulator.py
@@ -0,0 +1,380 @@
+from __future__ import annotations
+
+import logging
+import sys
+from enum import Enum
+
+from machine.isa import Opcode, Register, Word, dr, pc, read_code, sp, INPUT_PORT
+
+
+class Alu:
+    neg: bool = False
+    zero: bool = False
+
+    def __init__(self):
+        self.acc = 0
+
+    min_value = -(2**32)
+    max_value = 2**32 - 1
+
+    def set_flags(self, value: int):
+        if value > self.max_value:
+            value = value & self.min_value
+        if value < self.min_value:
+            value = value & self.min_value
+        self.neg = value < 0
+        self.zero = value == 0
+
+    def execute(self, opcode: Opcode, arg1: int, arg2: int = 0) -> int:
+        res: int
+        operations = {
+            Opcode.ADD: lambda a, b: a + b,
+            Opcode.ADD_LIT: lambda a, b: a + b,
+            Opcode.INC: lambda a, b: a + 1,
+            Opcode.DEC: lambda a, b: a - 1,
+            Opcode.XOR: lambda a, b: a ^ b,
+            Opcode.AND: lambda a, b: a & b,
+            Opcode.OR: lambda a, b: a | b,
+            Opcode.NEG: lambda a, b: ~a,
+            Opcode.DIV: lambda a, b: a / b,
+            Opcode.MOD: lambda a, b: a % b
+        }
+        operation = operations.get(opcode, None)
+        if operation is not None:
+            res = operation(arg1, arg2)
+        else:
+            raise ValueError("unknown opcode: {}".format(opcode))
+        self.set_flags(res)
+        return res
+
+
+class DataPath:
+    registers: dict[Register, int]
+
+    mem_size: int
+
+    memory: list[Word]
+
+    input_tokens: list[str]
+
+    alu: Alu = None
+
+    def __init__(self, data: list[Word], input_tokens: list[str], mem_size: int = 4096):
+        self.registers = {}
+        self.output_ports = {}
+        self.mem_size = mem_size
+        self.memory = data
+        self.input_tokens = input_tokens
+        self.alu = Alu()
+        for reg_num in range(0, 15):
+            self.registers[Register(reg_num)] = 0
+        self.registers[sp] = self.mem_size - 1
+
+    def latch_reg(self, reg: Register, value: int):
+        self.registers[reg] = value
+
+    def load_reg(self, reg: Register) -> int:
+        return self.registers[reg]
+
+    def get_instruction(self, addr: int) -> Word:
+        return self.memory[addr]
+
+    def memory_perform(self, oe: bool, wr: bool, addr: int) -> Word | None:
+        instr: Word = self.get_instruction(addr)
+        if oe:
+            return instr
+        if wr:
+            instr.arg1 = self.registers[dr]
+        if addr == INPUT_PORT:
+            symbol = self.input_tokens.pop(0)
+            logging.debug(f"input: {symbol!r} <- {''.join(self.input_tokens)!r}")
+
+        return None
+
+    def perform_arithmetic(self, opcode: Opcode, arg1: int, arg2: int = 0) -> int:
+        return self.alu.execute(opcode, arg1, arg2)
+
+    def zero(self) -> bool:
+        return self.alu.zero
+
+    def neg(self) -> bool:
+        return self.alu.neg
+
+
+class ControlUnit:
+    data_path: DataPath = None
+
+    tick_cnt: int = 0
+
+    def __init__(self, data_path):
+        self.tick_cnt = 0
+        self.data_path = data_path
+
+    def tick(self):
+        self.tick_cnt += 1
+
+    def current_tick(self):
+        return self.tick_cnt
+
+    def decode_and_execute_control_flow_instruction(self, instr, opcode) -> bool:
+        if opcode is Opcode.HALT:
+            raise StopIteration()
+
+        if opcode is Opcode.JUMP:
+            addr = instr.arg1
+            self.data_path.latch_reg(pc, addr)
+            self.tick()
+            return True
+
+        jmp_flag: bool = False
+
+        if (
+            opcode is Opcode.JE
+            or opcode is Opcode.JNE
+            or opcode is Opcode.JL
+            or opcode is Opcode.JLE
+            or opcode is Opcode.JG
+            or opcode is Opcode.JGE
+        ):
+            match opcode:
+                case Opcode.JE:
+                    jmp_flag = self.data_path.zero() == 1
+                case Opcode.JNE:
+                    jmp_flag = self.data_path.zero() == 0
+                case Opcode.JG:
+                    jmp_flag = self.data_path.neg() == 0
+                case Opcode.JGE:
+                    jmp_flag = self.data_path.neg() == 0 or self.data_path.zero() == 1
+                case Opcode.JL:
+                    jmp_flag = self.data_path.neg() == 1
+                case Opcode.JLE:
+                    jmp_flag = self.data_path.neg() == 1 or self.data_path.zero() == 1
+            if jmp_flag:
+                self.data_path.latch_reg(pc, instr.arg1)
+            else:
+                self.data_path.latch_reg(pc, self.data_path.registers[pc] + 1)
+            self.tick()
+            return True
+        return False
+
+    def fetch_instruction(self) -> Word:
+        addr: int = self.data_path.registers[pc]
+        instr: Word = self.data_path.memory[addr]
+        self.data_path.latch_reg(dr, instr.arg1)
+        self.tick()
+        return instr
+
+    def ld_addr(self, instr: Word):
+        addr: int = instr.arg2
+        reg: Register = instr.arg1
+        data: Word = self.data_path.memory_perform(True, False, addr)
+        self.data_path.latch_reg(dr, data.arg1)
+        self.tick()
+        self.data_path.latch_reg(reg, data.arg1)
+        self.tick()
+
+    def ld_lit(self, instr: Word):
+        val: int = instr.arg2
+        reg: Register = instr.arg1
+        self.data_path.latch_reg(reg, val)
+        self.tick()
+
+    def ld(self, instr: Word):
+        reg_to: Register = instr.arg1
+        addr_reg: Register = instr.arg2
+        addr_to_read: int = self.data_path.perform_arithmetic(Opcode.ADD, 0, self.data_path.registers[addr_reg])
+        self.tick()
+        data: Word = self.data_path.memory_perform(True, False, addr_to_read)
+        self.data_path.latch_reg(dr, data.arg1)
+        self.tick()
+        self.data_path.latch_reg(reg_to, data.arg1)
+        self.tick()
+
+    def st_addr(self, instr: Word):
+        addr: int = instr.arg2
+        reg: Register = instr.arg1
+        reg_data: int = self.data_path.perform_arithmetic(Opcode.ADD, 0, self.data_path.registers[reg])
+        self.tick()
+        self.data_path.latch_reg(dr, reg_data)
+        self.tick()
+        self.data_path.memory_perform(False, True, addr)
+        self.tick()
+
+    def st(self, instr: Word):
+        data_reg: Register = instr.arg1
+        data: int = self.data_path.perform_arithmetic(Opcode.ADD, 0, self.data_path.registers[data_reg])
+        self.tick()
+        self.data_path.latch_reg(dr, data)
+        self.tick()
+        addr_reg: Register = instr.arg2
+        addr: int = self.data_path.perform_arithmetic(Opcode.ADD, 0, self.data_path.registers[addr_reg])
+        self.tick()
+        self.data_path.memory_perform(False, True, addr)
+        self.tick()
+
+    def st_stack(self, instr: Word):
+        reg_from: Register = instr.arg1
+        data: int = self.data_path.perform_arithmetic(Opcode.ADD, 0, self.data_path.registers[reg_from])
+        self.data_path.latch_reg(dr, data)
+        self.tick()
+        addr_to: int = self.data_path.mem_size - instr.arg2 - 1
+        self.tick()
+        self.data_path.memory_perform(False, True, addr_to)
+        self.tick()
+
+    def mv(self, instr: Word):
+        reg_from: Register = instr.arg1
+        reg_from_data: int = self.data_path.perform_arithmetic(Opcode.ADD, 0, self.data_path.registers[reg_from])
+        self.tick()
+        reg_to: Register = instr.arg2
+        self.data_path.latch_reg(reg_to, reg_from_data)
+        self.tick()
+
+    def arythm(self, instr: Word):
+        res: int = self.data_path.perform_arithmetic(
+            instr.opcode, self.data_path.load_reg(instr.arg1), self.data_path.load_reg(instr.arg2)
+        )
+        self.data_path.latch_reg(instr.arg1, res)
+        self.tick()
+
+    def add_lit(self, instr: Word):
+        res: int = self.data_path.perform_arithmetic(instr.opcode, self.data_path.load_reg(instr.arg1), instr.arg2)
+        self.data_path.latch_reg(instr.arg1, res)
+        self.tick()
+
+    def cmp(self, instr: Word):
+        inv = self.data_path.perform_arithmetic(Opcode.NEG, self.data_path.load_reg(instr.arg2))
+        self.tick()
+        inv = self.data_path.perform_arithmetic(Opcode.INC, inv)
+        self.tick()
+        self.data_path.perform_arithmetic(Opcode.ADD, self.data_path.load_reg(instr.arg1), inv)
+        self.tick()
+
+    def sub(self, instr: Word):
+        inv = self.data_path.perform_arithmetic(Opcode.NEG, self.data_path.load_reg(instr.arg2))
+        self.tick()
+        inv = self.data_path.perform_arithmetic(Opcode.INC, inv)
+        self.tick()
+        res = self.data_path.perform_arithmetic(Opcode.ADD, self.data_path.load_reg(instr.arg1), inv)
+        self.data_path.latch_reg(instr.arg1, res)
+        self.tick()
+
+    def div(self, instr: Word):
+        res = self.data_path.perform_arithmetic(Opcode.DIV, self.data_path.load_reg(instr.arg1),
+                                                self.data_path.load_reg(instr.arg2))
+        self.data_path.latch_reg(instr.arg1, res)
+        self.tick()
+
+    def mod(self, instr: Word):
+        res = self.data_path.perform_arithmetic(Opcode.DIV, self.data_path.load_reg(instr.arg1),
+                                                self.data_path.load_reg(instr.arg2))
+        self.data_path.latch_reg(instr.arg1, res)
+        self.tick()
+
+    def unary_arythm(self, instr: Word):
+        res: int = self.data_path.perform_arithmetic(instr.opcode, self.data_path.load_reg(instr.arg1))
+        self.data_path.latch_reg(instr.arg1, res)
+        self.tick()
+
+    def decode_and_execute_instruction(self):
+        instr: Word = self.fetch_instruction()
+        opcode: Opcode = instr.opcode
+        opcode_mapping = {
+            Opcode.LD_ADDR: self.ld_addr,
+            Opcode.LD_LIT: self.ld_lit,
+            Opcode.LD: self.ld,
+            Opcode.ST_ADDR: self.st_addr,
+            Opcode.ST: self.st,
+            Opcode.MV: self.mv,
+            Opcode.ADD: self.arythm,
+            Opcode.OR: self.arythm,
+            Opcode.AND: self.arythm,
+            Opcode.XOR: self.arythm,
+            Opcode.INC: self.unary_arythm,
+            Opcode.DEC: self.unary_arythm,
+            Opcode.ADD_LIT: self.add_lit,
+            Opcode.CMP: self.cmp,
+            Opcode.SUB: self.sub,
+            Opcode.DIV: self.div,
+            Opcode.MOD: self.mod,
+            Opcode.NEG: self.unary_arythm,
+        }
+        if self.decode_and_execute_control_flow_instruction(instr, opcode):
+            return
+        if opcode in opcode_mapping:
+            opcode_mapping[opcode](instr)
+
+        self.data_path.latch_reg(pc, self.data_path.registers[pc] + 1)
+        self.tick()
+
+    def print_val_if_enum(self, value):
+        if isinstance(value, Enum):
+            return value.name
+        return value
+
+    def __repr__(self):
+        formatted_registers = {f"r{register.value}": value for register, value in self.data_path.registers.items()}
+        formatted_string = ", ".join([f"'{key}': {value}" for key, value in formatted_registers.items()])
+
+        state_repr = "TICK: {:3} PC: {:3}  MEM_OUT: {} {} reg: {}".format(
+            self.tick_cnt,
+            self.data_path.registers[pc],
+            self.print_val_if_enum(self.data_path.memory[self.data_path.registers[pc]].arg1),
+            self.print_val_if_enum(self.data_path.memory[self.data_path.registers[pc]].arg2),
+            formatted_string,
+        )
+
+        instr = self.data_path.memory[self.data_path.registers[pc]]
+        opcode = str(instr.opcode)
+
+        instr_repr = "  ('{}'@{}:{} {})".format(instr.index, opcode, instr.arg1, instr.arg2)
+
+        return "{} \t{}".format(state_repr, instr_repr)
+
+
+def simulation(mem: list[Word], input_tokens: list[str], limit: int):
+    data_path = DataPath(mem, input_tokens)
+    control_unit = ControlUnit(data_path)
+    instr_counter = 0
+
+    logging.debug("%s", control_unit)
+    try:
+        while instr_counter < limit:
+            control_unit.decode_and_execute_instruction()
+            instr_counter += 1
+            logging.debug("%s", control_unit)
+    except EOFError:
+        logging.warning("Input buffer is empty!")
+    except StopIteration:
+        pass
+
+    if instr_counter >= limit:
+        logging.warning("Limit exceeded!")
+    logging.info("output_buffer: %s", repr("".join(data_path.output_ports[0])))
+    return "".join(data_path.output_ports[0]), instr_counter, control_unit.current_tick()
+
+
+def main(code_file, input_file):
+    code: list[Word] = read_code(code_file)
+    with open(input_file, encoding="utf-8") as file:
+        input_text = file.read()
+        input_token = []
+        for char in input_text:
+            input_token.append(char)
+        input_token.insert(0, str(len(input_token)))
+
+    output, instr_counter, ticks = simulation(
+        code,
+        input_tokens=input_token,
+        limit=100000,
+    )
+
+    print("".join(output))
+    print("instr_counter: ", instr_counter, "ticks:", ticks)
+
+
+if __name__ == "__main__":
+    logging.getLogger().setLevel(logging.DEBUG)
+    assert len(sys.argv) == 3, "Wrong arguments: emulator.py <code_file> <input_file>"
+    _, code_file, input_file = sys.argv
+    main(code_file, input_file)
